2f1f581cf14ea4e6b70c51ec7139f6597c518491
Vivian Nguyen <vivian@tiledb.com>
2024-08-01 12:13:49 -0500

    Add spatial to Experiment

--- apis/python/src/tiledbsoma/_experiment.py
+++ apis/python/src/tiledbsoma/_experiment.py
@@ -16,6 +16,7 @@ from ._collection import Collection, CollectionBase
 from ._dataframe import DataFrame
 from ._indexer import IntIndexer
 from ._measurement import Measurement
+from ._scene import Scene
 from ._soma_object import AnySOMAObject
 
 
@@ -24,6 +25,7 @@ class Experiment(  # type: ignore[misc]  # __eq__ false positive
     experiment.Experiment[  # type: ignore[type-var]
         DataFrame,
         Collection[Measurement],
+        Collection[Scene],
         AnySOMAObject,
     ],
 ):
@@ -69,6 +71,7 @@ class Experiment(  # type: ignore[misc]  # __eq__ false positive
     _subclass_constrained_soma_types = {
         "obs": ("SOMADataFrame",),
         "ms": ("SOMACollection",),
+        "spatial": ("SOMACollection",),
     }
 
     def axis_query(  # type: ignore
