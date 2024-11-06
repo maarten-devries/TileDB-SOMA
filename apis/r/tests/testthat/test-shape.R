4a44022c2d96f37eb00dc00206407bd7350fea68
John Kerl <kerl.john.r@gmail.com>
2024-11-06 20:31:42 +0000

    update a few existing unit-test cases

--- apis/r/tests/testthat/test-shape.R
+++ apis/r/tests/testthat/test-shape.R
@@ -357,14 +357,14 @@ test_that("SOMADataFrame domain mods", {
    index_column_names <- c("soma_joinid", "mystring", "myint", "myfloat")
 
     domain_for_create <- list(
-        soma_joinid = c(1, 4),
+        soma_joinid = c(0, 3),
         mystring = NULL,
         myint = c(20, 50),
         myfloat = c(0.0, 6.0)
     )
 
     table <- arrow::arrow_table(
-      soma_joinid = 1L:4L,
+      soma_joinid = 0L:3L,
       mystring = c("a", "b", "a", "b"),
       myint = c(20, 30, 40, 50),
       myfloat = c(1.0, 2.5, 4.0, 5.5),
@@ -385,7 +385,7 @@ test_that("SOMADataFrame domain mods", {
 
     # Check "expand" to same
     new_domain <- list(
-        soma_joinid = c(1, 4),
+        soma_joinid = c(0, 3),
         mystring = NULL,
         myint = c(20, 50),
         myfloat = c(0.0, 6.0)
@@ -394,7 +394,7 @@ test_that("SOMADataFrame domain mods", {
 
     # Shrink
     new_domain <- list(
-        soma_joinid = c(1, 3),
+        soma_joinid = c(0, 2),
         mystring = NULL,
         myint = c(20, 50),
         myfloat = c(0.0, 6.0)
@@ -402,7 +402,7 @@ test_that("SOMADataFrame domain mods", {
     expect_error(sdf$change_domain(new_domain))
 
     new_domain <- list(
-        soma_joinid = c(1, 4),
+        soma_joinid = c(0, 3),
         mystring = NULL,
         myint = c(20, 40),
         myfloat = c(0.0, 6.0)
@@ -410,7 +410,7 @@ test_that("SOMADataFrame domain mods", {
     expect_error(sdf$change_domain(new_domain))
 
     new_domain <- list(
-        soma_joinid = c(1, 4),
+        soma_joinid = c(0, 3),
         mystring = NULL,
         myint = c(20, 50),
         myfloat = c(2.0, 6.0)
@@ -419,7 +419,7 @@ test_that("SOMADataFrame domain mods", {
 
     # String domain cannot be specified
     new_domain <- list(
-        soma_joinid = c(1, 4),
+        soma_joinid = c(0, 3),
         mystring = c("a", "z"),
         myint = c(20, 50),
         myfloat = c(0.0, 6.0)
@@ -430,7 +430,7 @@ test_that("SOMADataFrame domain mods", {
 
     # String domain cannot be specified
     new_domain <- list(
-        soma_joinid = c(1, 9),
+        soma_joinid = c(0, 8),
         mystring = c("", ""),
         myint = c(20, 50),
         myfloat = c(0.0, 10.0)
